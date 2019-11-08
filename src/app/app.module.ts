75c963b5f1a1405ce631dc978b5bcb4410a96b70
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-08 16:40:59 +0700

    learn how to build quill editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QuillModule } from 'ngx-quill';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
